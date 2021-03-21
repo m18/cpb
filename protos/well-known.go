2076bc2aaca98efb5efa445bd1a2169a66fc7421
m18 <mikaelino@gmail.com>
2021-03-21 19:43:32 +1100

    add well-known types

--- protos/well-known.go
+++ protos/well-known.go
@@ -1,7 +1,16 @@
 package protos
 
 import (
-	// TODO: import well-known types
+	// import well-known types
 	// https://pkg.go.dev/google.golang.org/protobuf/types/known
+	_ "google.golang.org/protobuf/types/known/anypb"
+	_ "google.golang.org/protobuf/types/known/apipb"
+	_ "google.golang.org/protobuf/types/known/durationpb"
+	_ "google.golang.org/protobuf/types/known/emptypb"
+	_ "google.golang.org/protobuf/types/known/fieldmaskpb"
+	_ "google.golang.org/protobuf/types/known/sourcecontextpb"
+	_ "google.golang.org/protobuf/types/known/structpb"
 	_ "google.golang.org/protobuf/types/known/timestamppb"
+	_ "google.golang.org/protobuf/types/known/typepb"
+	_ "google.golang.org/protobuf/types/known/wrapperspb"
 )
