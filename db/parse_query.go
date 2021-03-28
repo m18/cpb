f218384e7e58276a9646a64a9bf98c7cbfca2e3f
m18 <mikaelino@gmail.com>
2021-03-28 16:54:51 +1100

    add queryParser.parseInMessageArgs and ctor

---
+++ db/parse_query.go
@@ -0,0 +1,103 @@
+package db
+
+import (
+	"fmt"
+	"regexp"
+	"strings"
+
+	"github.com/m18/cpb/config"
+	"github.com/m18/cpb/protos"
+	"github.com/m18/cpb/rx"
+)
+
+var inParamReplacers = map[string]func() func(string) string{
+	DriverPostgres: func() func(string) string {
+		counter := 0
+		return func(string) string {
+			counter = counter + 1
+			return fmt.Sprintf("$%d", counter)
+		}
+	},
+}
+
+type queryParser struct {
+	protos                         *protos.Protos
+	inMessages                     map[string]*config.InMessage
+	inParamReplacer                func() func(string) string
+	outMessages                    map[string]*config.OutMessage
+	inqueryrx, inargrx, outqueryrx *regexp.Regexp
+	normalizeInMessageArgs         func([]string) []string
+}
+
+func newQueryParser(driver string, p *protos.Protos, inMessages map[string]*config.InMessage, outMessages map[string]*config.OutMessage) *queryParser {
+	inargnormrx := regexp.MustCompile(`'((\\'|[^'])*)'`)
+	normalizer := func(args []string) []string {
+		for i, arg := range args {
+			if inargnormrx.MatchString(arg) {
+				arg = inargnormrx.ReplaceAllString(arg, "\"$1\"")
+				args[i] = strings.ReplaceAll(arg, "\\'", "'")
+			}
+		}
+		return args
+	}
+
+	return &queryParser{
+		protos:                 p,
+		inMessages:             inMessages,
+		inParamReplacer:        inParamReplacers[driver], // driver has already been validated
+		outMessages:            outMessages,
+		inqueryrx:              regexp.MustCompile(`\$(?P<alias>\w+)\((?P<args>((\s*('(\\'|[^'])*'|\d+(.\d+)?|true|false)\s*,)*(\s*('(\\'|[^'])*'|\d+(.\d+)?|true|false)\s*))|)\)`),
+		inargrx:                regexp.MustCompile(`'(\\'|[^'])*'|\d+(.\d+)?|true|false`),
+		outqueryrx:             regexp.MustCompile(`\$(?P<alias>\w+):(?P<col>\w+|"(\w+\s*)+")(?P<full_col_alias>(\s+[aA][sS])?\s+(?P<col_alias>\w+|"(\w+\s*)+")[\s,$])?`),
+		normalizeInMessageArgs: normalizer,
+	}
+}
+
+// func (p *queryParser) Parse(q string) (string, [][]byte, map[string]func([]byte) (string, error), error) {
+// 	var inMessageArgs [][]byte
+// 	var prettyPrinters map[string]func([]byte) (string, error)
+// 	var err error
+
+// 	if q, inMessageArgs, err = p.parseInMessageArgs(q); err != nil {
+// 		return "", nil, nil, err
+// 	}
+
+// 	if q, prettyPrinters, err = p.parseOutMessageArgs(q); err != nil {
+// 		return "", nil, nil, err
+// 	}
+
+// 	return q, inMessageArgs, prettyPrinters, nil
+// }
+
+func (p *queryParser) parseInMessageArgs(q string) (string, [][]byte, error) {
+	groups, ok := rx.FindAllGroups(p.inqueryrx, q)
+	if !ok {
+		// ok to have no "in" messages
+		return q, nil, nil
+	}
+	queryArgs := make([][]byte, 0, len(groups))
+	for _, group := range groups {
+		alias := group["alias"]
+		inMessage, ok := p.inMessages[alias]
+		if !ok {
+			return "", nil, fmt.Errorf("unknown alias in query: %q", alias)
+		}
+
+		args, _ := rx.FindAllMatches(p.inargrx, group["args"]) // ignoring ok as it's been verified by p.inqueryrx already; treat empty () as ok too
+		args = p.normalizeInMessageArgs(args)
+		jsonMessage, err := inMessage.JSON(args)
+		if err != nil {
+			return "", nil, err
+		}
+		queryArg, err := p.protos.ProtoBytes(inMessage.Name, jsonMessage)
+		if err != nil {
+			return "", nil, err
+		}
+
+		queryArgs = append(queryArgs, queryArg)
+	}
+
+	query := p.inqueryrx.ReplaceAllStringFunc(q, p.inParamReplacer())
+
+	return query, queryArgs, nil
+}
---
+++ db/parse_query_test.go
@@ -0,0 +1,220 @@
+package db
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/m18/cpb/check"
+	"github.com/m18/cpb/internal/testconfig"
+	"github.com/m18/cpb/internal/testprotos"
+)
+
+func TestQueryParserNormalizeInMessageArgs(t *testing.T) {
+	tests := []struct {
+		args         []string
+		expectedArgs []string
+	}{
+		{
+			args:         []string{},
+			expectedArgs: []string{},
+		},
+		{
+			args:         []string{""},
+			expectedArgs: []string{""},
+		},
+		{
+			args:         []string{"", ""},
+			expectedArgs: []string{"", ""},
+		},
+		{
+			args:         []string{"'foo'", "1"},
+			expectedArgs: []string{"\"foo\"", "1"},
+		},
+		{
+			args:         []string{"0", "1"},
+			expectedArgs: []string{"0", "1"},
+		},
+		{
+			args:         []string{"'foo'", "'bar'"},
+			expectedArgs: []string{"\"foo\"", "\"bar\""},
+		},
+		{
+			args:         []string{"'foo bar'", "' bar baz '"},
+			expectedArgs: []string{"\"foo bar\"", "\" bar baz \""},
+		},
+		{
+			args:         []string{"'o\\'foo o\\'bar'", "' bar o\\'baz '"},
+			expectedArgs: []string{"\"o'foo o'bar\"", "\" bar o'baz \""},
+		},
+	}
+	qp := newQueryParser(DriverPostgres, nil, nil, nil)
+	for _, test := range tests {
+		test := test
+		t.Run(fmt.Sprint(test.args), func(t *testing.T) {
+			t.Parallel()
+			args := qp.normalizeInMessageArgs(test.args)
+			if !check.StringSlicesAreEqual(args, test.expectedArgs) {
+				t.Fatalf("expected %v but got %v", test.expectedArgs, args)
+			}
+		})
+	}
+
+}
+
+func TestQueryParserParseInMessageArgs(t *testing.T) {
+	p, err := testprotos.MakeProtosLite()
+	if err != nil {
+		t.Fatal(err)
+	}
+	tests := []struct {
+		desc             string
+		driver           string
+		query            string
+		expectedQuery    string
+		expectedArgCount int
+		err              bool
+	}{
+		{
+			desc:             "valid, no params",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = 100",
+			expectedQuery:    "select * from test where foo_col = 100",
+			expectedArgCount: 0,
+		},
+		{
+			desc:             "valid, malformed param",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1",
+			expectedQuery:    "select * from test where foo_col = $foo(1",
+			expectedArgCount: 0,
+		},
+		{
+			desc:             "valid, single param",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1, 'one')",
+			expectedQuery:    "select * from test where foo_col = $1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, single param, type coersion - '1' -> 1",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo('1', 'one')",
+			expectedQuery:    "select * from test where foo_col = $1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, single param, extra spaces",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col   =  $foo(  1 , 'one'  )  ",
+			expectedQuery:    "select * from test where foo_col   =  $1  ",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, single param, no spaces",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col=$foo(1,'one')",
+			expectedQuery:    "select * from test where foo_col=$1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, single param, no left space",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col= $foo(1,'one')",
+			expectedQuery:    "select * from test where foo_col= $1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, single param, no right space",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col =$foo(1,'one')",
+			expectedQuery:    "select * from test where foo_col =$1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, multiple params",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1, 'one') and bar_col = $bar(2, 'two')",
+			expectedQuery:    "select * from test where foo_col = $1 and bar_col = $2",
+			expectedArgCount: 2,
+		},
+		{
+			desc:             "valid, multiple params, mixed spaces",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col=$foo(   1   ,  'one'   )  and   bar_col =$bar( 2, 'two' ) ",
+			expectedQuery:    "select * from test where foo_col=$1  and   bar_col =$2 ",
+			expectedArgCount: 2,
+		},
+		{
+			desc:             "valid, single param, escaped quote",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1, 'o\\'one')",
+			expectedQuery:    "select * from test where foo_col = $1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:             "valid, multiple params, escaped quotes",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1, 'o\\'one') and bar_col = $bar(2, 'o\\'two')",
+			expectedQuery:    "select * from test where foo_col = $1 and bar_col = $2",
+			expectedArgCount: 2,
+		},
+		{
+			desc:             "valid, JSON-escaped double quote",
+			driver:           DriverPostgres,
+			query:            "select * from test where foo_col = $foo(1, 'one \\\" two')",
+			expectedQuery:    "select * from test where foo_col = $1",
+			expectedArgCount: 1,
+		},
+		{
+			desc:   "invalid, JSON-unescaped double quote",
+			driver: DriverPostgres,
+			query:  "select * from test where foo_col = $foo(1, 'one \" two')",
+			err:    true,
+		},
+		{
+			desc:   "invalid, single param, wrong arg type",
+			driver: DriverPostgres,
+			query:  "select * from test where foo_col = $foo('not a number', 'one')",
+			err:    true,
+		},
+		{
+			desc:   "invalid, single param, wrong arg count",
+			driver: DriverPostgres,
+			query:  "select * from test where foo_col = $foo(1)",
+			err:    true,
+		},
+		{
+			desc:   "invalid, multiple params, wrong arg type",
+			driver: DriverPostgres,
+			query:  "select * from test where foo_col = $foo(1, 'one') and bar_col = $bar('not a number', 'two')",
+			err:    true,
+		},
+		{
+			desc:   "invalid, multiple params, wrong arg count",
+			driver: DriverPostgres,
+			query:  "select * from test where foo_col = $foo(1, 'one') and bar_col = $bar(2)",
+			err:    true,
+		},
+	}
+	for _, test := range tests {
+		test := test
+		t.Run(test.desc, func(t *testing.T) {
+			t.Parallel()
+			cfg, err := testconfig.MakeTestConfigLite(test.driver)
+			if err != nil {
+				t.Fatal(err)
+			}
+			qp := newQueryParser(cfg.DB.Driver, p, cfg.InMessages, nil)
+			q, args, err := qp.parseInMessageArgs(test.query)
+			if err == nil == test.err {
+				t.Fatalf("expected %t but did not get it: %v", test.err, err)
+			}
+			if q != test.expectedQuery {
+				t.Fatalf("expected query to be %q but it was %q", test.expectedQuery, q)
+			}
+			if len(args) != test.expectedArgCount {
+				t.Fatalf("expected arg count to be %d but it was %d", test.expectedArgCount, len(args))
+			}
+		})
+	}
+}
